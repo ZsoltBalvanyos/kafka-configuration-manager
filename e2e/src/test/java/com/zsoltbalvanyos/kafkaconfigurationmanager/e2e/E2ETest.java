9744746c0d6a87be4c828a454e7f2f5d1fa67491
Zsolt Balvanyos <zsolt.balvanyos@gmail.com>
2021-04-25 10:54:02 +0100

    Use java 8 in e2e test

--- e2e/src/test/java/com/zsoltbalvanyos/kafkaconfigurationmanager/e2e/E2ETest.java
+++ e2e/src/test/java/com/zsoltbalvanyos/kafkaconfigurationmanager/e2e/E2ETest.java
@@ -2,8 +2,7 @@ package com.zsoltbalvanyos.kafkaconfigurationmanager.e2e;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 import org.apache.kafka.clients.admin.*;
@@ -20,15 +19,16 @@ public class E2ETest {
 
   private final Logger log = LoggerFactory.getLogger(getClass().getName());
   private final String kafkaEndpoint = "10.5.0.5:9093";
-  private final Admin admin =
-      Admin.create(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093"));
 
   @Test
   public void test() throws Exception {
+    Properties properties = new Properties();
+    properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093");
+    Admin admin = Admin.create(properties);
 
     new GenericContainer<>(DockerImageName.parse("kafka-configuration-manager:latest"))
         .waitingFor(Wait.forLogMessage(".*(Kafka Configuration Manager exited).*", 1))
-        .withEnv(Map.of("BOOTSTRAP_SERVER", kafkaEndpoint))
+        .withEnv("BOOTSTRAP_SERVER", kafkaEndpoint)
         .withFileSystemBind(
             "./src/test/resources/plaintext.properties", "/properties/command-config.properties")
         .withFileSystemBind("./src/test/resources/test-config.yml", "/config/configuration.yml")
@@ -37,20 +37,22 @@ public class E2ETest {
         .withNetworkMode("host")
         .start();
 
-    var topicNames =
+    List<String> topicNames =
         admin.listTopics().listings().get().stream()
             .map(TopicListing::name)
             .collect(Collectors.toList());
 
     assertThat(topicNames).containsExactlyInAnyOrder("orders.pizza.0", "orders.coffee.2");
-    assertThat(getConfigEntries("orders.pizza.0").get("flush.messages").value()).isEqualTo("100");
-    assertThat(getConfigEntries("orders.pizza.0").get("unclean.leader.election.enable").value())
+    assertThat(getConfigEntries("orders.pizza.0", admin).get("flush.messages").value())
+        .isEqualTo("100");
+    assertThat(
+            getConfigEntries("orders.pizza.0", admin).get("unclean.leader.election.enable").value())
         .isEqualTo("false");
   }
 
-  private Config getConfigEntries(String topicName)
+  private Config getConfigEntries(String topicName, Admin admin)
       throws ExecutionException, InterruptedException {
-    var configResource = new ConfigResource(ConfigResource.Type.TOPIC, topicName);
-    return admin.describeConfigs(List.of(configResource)).all().get().get(configResource);
+    ConfigResource configResource = new ConfigResource(ConfigResource.Type.TOPIC, topicName);
+    return admin.describeConfigs(Arrays.asList(configResource)).all().get().get(configResource);
   }
 }
